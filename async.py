8468cac8dded9a66115d5d70c94e27787b84ce57
Roger Howard <rogerhoward@mac.com>
2016-09-26 10:04:54 -0700

    Refresh data

---
+++ async.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import getty
+from pprint import pprint
+from tasks import get_object
+
+
+if __name__ == '__main__':
+    for objid in range(1, 1000):
+        get_object.delay(objid)
